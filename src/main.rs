b61e5be6912cbc3f765b50216ab5b02013b5c2cd
krxi <maplansinthere@proton.me>
2025-11-24 01:36:59 +0300

    Refactor run.sh for error handling and improve code structure in main.rs and tcp.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-use std::io;
-use tun_tap::*;
-use etherparse::{Ipv4HeaderSlice, TcpHeaderSlice};
+use etherparse::{Ipv4HeaderSlice, TcpHeader, TcpHeaderSlice};
 use std::collections::HashMap;
+use std::io;
 use std::net::Ipv4Addr;
+use tun_tap::*;
 
 mod tcp;
 
-#[derive(Clone,Copy,Eq,PartialEq,Hash,Debug)] // I explained in my notes
-struct Quad { // Its structure of each Connection.
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)] // I explained in my notes
+struct Quad {
+    // Its structure of each Connection.
     // Ipv4,Port
-    src: (Ipv4Addr,u16), // I used Ipv4Addr function because the library gives that type
-    dst: (Ipv4Addr,u16)  // And its easy to keep understand, Keeps octets [u8; 4].
+    src: (Ipv4Addr, u16), // I used Ipv4Addr function because the library gives that type
+    dst: (Ipv4Addr, u16), // And its easy to keep understand, Keeps octets [u8; 4].
 }
 
 fn main() -> io::Result<()> {
     let mut connections: HashMap<Quad, tcp::State> = HashMap::new();
-    
+
     let a = 5;
     let b = 4;
-    
 
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?; //we created normal virtual network instance
 
@@ -27,9 +27,9 @@ fn main() -> io::Result<()> {
     loop {
         let bytes = nic.recv(&mut buf[..])?; // the func needed with mut buf |--| pub fn  recv(&self, buf: &mut [u8]) -> Result<usize> |--|
 
-        let flags = i16::from_be_bytes([buf[0],buf[1]]); // Takes the first 2 bytes in bytes array and converts to integer ( decimal )
-        let eth_proto = i16::from_be_bytes([buf[2],buf[3]]); // Takes the second 2 bytes in bytes array and converts to integer ( decimal )
-        if eth_proto != 0x800 {
+        let flags = i16::from_be_bytes([buf[0], buf[1]]); // Takes the first 2 bytes in bytes array and converts to integer ( decimal )
+        let eth_proto = i16::from_be_bytes([buf[2], buf[3]]); // Takes the second 2 bytes in bytes array and converts to integer ( decimal )
+        if eth_proto != 0x0800 {
             // not ipv4
             continue;
         }
@@ -37,49 +37,52 @@ fn main() -> io::Result<()> {
         // IP LAYER
         match Ipv4HeaderSlice::from_slice(&buf[4..bytes]) {
             Ok(iph) => {
-                
-               
                 // At the first, I used payload_len()
-                // I misunderstood to get length of protocol header size why its have Result type ? 
+                // I misunderstood to get length of protocol header size why its have Result type ?
                 // because this function returns the payload size with checking max header size of ipv4 datagram
                 // so i changed with p.slice().len() its returns the real header size
 
                 let p_len = iph.slice().len(); // protocol len
                 let src = iph.source_addr();
                 let dst = iph.destination_addr();
-                
-                if iph.protocol() != 0x06 {
+
+                if iph.protocol().0 != 0x06 {
                     // not tcp
                     continue;
                 }
 
-                match TcpHeaderSlice::from_slice(&buf[4 + p_len..]) { //Skipping the header of IPV4 Datagram Header
+                match TcpHeaderSlice::from_slice(&buf[4 + p_len..bytes]) {
+                    // Skipping the header of IPV4 Datagram Header
                     Ok(tcph) => {
                         let data = 4 + p_len + tcph.slice().len();
 
-                        //connections.entry(Quad { src: (src,h.0.source_port), dst: (dst,h.0.destination_port) })
-                        println!("{} -> {}, {}b of tcp to port {}",src,dst,tcph.slice().len(),tcph.destination_port())
+                        connections
+                            .entry(Quad {
+                                src: (src, tcph.source_port()),
+                                dst: (dst, tcph.destination_port()),
+                            })
+                            .or_default()
+                            .on_packet(iph, tcph, &buf[data..bytes]);
                     }
                     Err(e) => {
-                        println!("Ignoring weird tcp packet: {:?}",e)
+                        println!("Ignoring weird tcp packet: {:?}", e)
                     }
                 }
-                
-            },
+            }
             Err(e) => {
-                println!("Ignoring weird packet: {:?}",e)
+                println!("Ignoring weird packet: {:?}", e)
             }
         }
 
-        //println!("read {} bytes: {:x?}", bytes - 4, &buf[4..bytes]); 
-                                                                    // and we changes bytes to bytes -4 (because we are deleting flags and proto for real total byte info)
-                                                                    // and we used the first 4 byte for finding flag and proto so we are starting with fourth byte
-                                                                    // &buf[..bytes] to &buf[4..bytes]
+        //println!("read {} bytes: {:x?}", bytes - 4, &buf[4..bytes]);
+        // and we changes bytes to bytes -4 (because we are deleting flags and proto for real total byte info)
+        // and we used the first 4 byte for finding flag and proto so we are starting with fourth byte
+        // &buf[..bytes] to &buf[4..bytes]
 
-                                                                    // we are writing &buf[..bytes] -> ..bytes because 
-                                                                    // rustlang automatic fill the vector automaticly with
-                                                                    // zeros and if we try to write every data
-                                                                    // the program write every 0 and its not efficient
+        // we are writing &buf[..bytes] -> ..bytes because
+        // rustlang automatic fill the vector automaticly with
+        // zeros and if we try to write every data
+        // the program write every 0 and its not efficient
     }
 
     Ok(())
--- src/tcp.rs
+++ src/tcp.rs
@@ -1,3 +1,52 @@
-pub struct State {
+use std::net::Ipv4Addr;
 
+use etherparse::{Ipv4Header, Ipv4HeaderSlice, TcpHeader, TcpHeaderSlice, ip_number::TCP};
+
+pub enum State {
+    Closed,
+    Listen,
+    SynRcvd,
+    SynSent,
+    Estab
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State::Listen
+    }
+}
+
+impl State {
+    pub fn on_packet<'a>(&mut self,iph: Ipv4HeaderSlice<'a>, tcph: TcpHeaderSlice, data: &'a [u8])
+    //Lifetime Annotation 'a 
+    // The datas which have 'a annotation, means they are same lifetime for avoid dangling pointer and data corruption
+    {
+        match self {
+            State::Closed => return,
+            State::Listen => {
+                if !tcph.syn() {
+                    //only excepted syn packet
+                    return
+                }
+
+                //need to start establish a connection
+                let mut tcp_packet = TcpHeader::new(tcph.source_port(), tcph.destination_port(), 0, 0);
+                tcp_packet.ack = true;
+                tcp_packet.syn = true;
+                let mut ipv4_packet = Ipv4Header::new(tcp_packet.header_len(), 64, TCP, iph.destination(),iph.source());
+
+            },
+            State::SynRcvd => todo!(),
+            State::SynSent => todo!(),
+            State::Estab => todo!(),
+        }
+        println!(
+            "{}:{} -> {}:{}, {}b of tcp",
+            iph.source_addr(),
+            tcph.source_port(),
+            iph.destination_addr(),
+            tcph.destination_port(),
+            data.len()
+        )
+    }
 }
